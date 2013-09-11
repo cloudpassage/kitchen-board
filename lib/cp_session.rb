6fe504b8e4e15cc1a6723a8ea842bb0385c81445
Alfonso Adriasola <aadriasola@cloudpassage.com>
2013-09-11 11:51:33 -0700

    request / accept json

--- lib/cp_session.rb
+++ lib/cp_session.rb
@@ -5,30 +5,31 @@ class CpSession
     client = OAuth2::Client.new(CLIENTID,
                                 CLIENTSECRET,
                                 :site => "https://#{HOST}",
-                                :token_url => '/oauth/access_token')
-
+                                :token_url => '/oauth/access_token'
+                                )
 
     @token = client.client_credentials.get_token.token
     @api_url = "https://#{HOST}/v1/"
     @auth = {'Authorization' => "Bearer #{@token}"}
+    @common = @auth.merge({:accept => :json, :content_type=>:json})
 
   end
 
   def get(endpoint)
-    CpResponse.new(RestClient.get @api_url+"#{endpoint}", @auth)
+    CpResponse.new(RestClient.get @api_url+"#{endpoint}", @common)
   end
 
   def post(endpoint, params)
-    CpResponse.new(RestClient.post @api_url+"#{endpoint}.json", params, @auth)
+    CpResponse.new(RestClient.post @api_url+"#{endpoint}", params, @common)
 
   end
 
   def put(endpoint, params)
-    CpResponse.new(RestClient.put @api_url+"#{endpoint}.json", params, @auth)
+    CpResponse.new(RestClient.put @api_url+"#{endpoint}", params, @common)
   end
 
   def delete(endpoint)
-    CpResponse.new(RestClient.delete @api_url+"#{endpoint}", @auth)
+    CpResponse.new(RestClient.delete @api_url+"#{endpoint}", @common)
   end
 
 end
