110b2d13b7842c5914c9a07faae6c194b344a3d6
Alfonso Adriasola <aadriasola@cloudpassage.com>
2014-05-19 10:05:57 -0700

    making it easier to print

--- lib/cp_session.rb
+++ lib/cp_session.rb
@@ -4,32 +4,29 @@ class CpSession
   def initialize
     client = OAuth2::Client.new(CLIENTID,
                                 CLIENTSECRET,
-                                :site => "https://#{HOST}",
-                                :token_url => '/oauth/access_token',
-                                :ssl =>{:verify=>false})
+                                site: "https://#{HOST}",
+                                token_url: '/oauth/access_token',
+                                ssl:  { verify: false })
 
     @token = client.client_credentials.get_token.token
     @api_url = "https://#{HOST}/v1/"
-    @auth = {'Authorization' => "Bearer #{@token}"}
-    @common = @auth.merge({:accept => :json, :content_type=>:json})
-
+    @auth = { 'Authorization' => "Bearer #{@token}" }
+    @common = @auth.merge(accept: :json, content_type: :json)
   end
 
-  def get(endpoint, params={})
-    CpResponse.new{RestClient.get @api_url+"#{endpoint}", @common.merge(params:params)}
+  def get(endpoint, params = {})
+    CpResponse.new { RestClient.get @api_url + "#{endpoint}", @common.merge(params: params) }
   end
 
   def post(endpoint, params)
-    CpResponse.new{RestClient.post @api_url+"#{endpoint}", params, @common}
-
+    CpResponse.new { RestClient.post @api_url + "#{endpoint}", params, @common }
   end
 
   def put(endpoint, params)
-    CpResponse.new{RestClient.put @api_url+"#{endpoint}", params, @common}
+    CpResponse.new { RestClient.put @api_url + "#{endpoint}", params, @common }
   end
 
   def delete(endpoint)
-    CpResponse.new{RestClient.delete @api_url+"#{endpoint}", @common}
+    CpResponse.new { RestClient.delete @api_url + "#{endpoint}", @common }
   end
-
 end
