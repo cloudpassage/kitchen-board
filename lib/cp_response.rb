af30f83f6c067bc07bafbe97b131c24ff4ff2c68
Alfonso Adriasola <aadriasola@cloudpassage.com>
2014-10-24 15:54:11 -0700

    remove pygments, allow v2 ruby colorize

--- lib/cp_response.rb
+++ lib/cp_response.rb
@@ -6,17 +6,28 @@ class CpResponse
     @result = e
   end
 
-  def pretty
-    json = JSON.pretty_unparse(JSON.parse(@result))
-    Pygments.highlight(json, :formatter => 'terminal', :lexer => 'json', :options => {:encoding => 'utf-8'})
-  end
-
   def to_hash
     JSON.parse(@result)
+  end
+
+  def json
+    json = JSON.pretty_unparse(JSON.parse(@result))
+  end
+
+  def highlighted
+    CodeRay.scan(json, :json)
+  end  
+
+  def pretty
+    highlighted.terminal
   end
 
   def to_s
     return pretty if @result.is_a? String
     @result.inspect
   end
+
+  def html
+    puts CodeRay.scan(json, :json).div
+  end   
 end
