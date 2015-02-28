8837e2a99cb52016e02b7a57bfc33d385ad2ff55
Alfonso Adriasola <aadriasola@cloudpassage.com>
2015-02-27 16:40:46 -0800

    * add some error handling and better parameters use

--- lib/cp_session.rb
+++ lib/cp_session.rb
@@ -1,4 +1,3 @@
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 class CpSession
   include CpConfig
   attr_accessor :version
@@ -9,33 +8,36 @@ class CpSession
                                 :site => "https://#{HOST}",
                                 :token_url => '/oauth/access_token',
                                 :ssl_verify => false
-                                )
+    )
 
     @token = client.client_credentials.get_token.token
     @api_url = "https://#{HOST}/"
-    @auth = { 'Authorization' => "Bearer #{@token}" }
-    @common = @auth.merge(accept: :json, content_type: :json)
+    @common = {
+        accept: :json,
+        content_type: :json,
+        'Authorization' => "Bearer #{@token}"
+    }
     @version = "1"
   end
 
   def get(endpoint, params = {})
     v = params.delete(:version) || @version
-    CpResponse.new { RestClient.get @api_url + "v#{v}/#{endpoint}", @common.merge(params: params) }
+    CpResponse.new { RestClient.get @api_url + "v#{v}/#{endpoint}", @common.merge(params) }
   end
 
-  def post(endpoint, params)
+  def post(endpoint, params = {})
     v = params.delete(:version) || @version
-    CpResponse.new { RestClient.post @api_url + "v#{v}/#{endpoint}", params, @common }
+    CpResponse.new { RestClient.post @api_url + "v#{v}/#{endpoint}", @common.merge(params) }
   end
 
-  def put(endpoint, params)
+  def put(endpoint, params = {})
     v = params.delete(:version) || @version
-    CpResponse.new { RestClient.put @api_url + "v#{v}/#{endpoint}", params, @common }
+    CpResponse.new { RestClient.put @api_url + "v#{v}/#{endpoint}", @common.merge(params) }
   end
 
-  def delete(endpoint, params)
+  def delete(endpoint, params = {})
     v = params.delete(:version) || @version
-    CpResponse.new { RestClient.delete @api_url + "v#{v}/#{endpoint}", @common }
+    CpResponse.new { RestClient.delete @api_url + "v#{v}/#{endpoint}", @common.merge(params) }
   end
 
 end
